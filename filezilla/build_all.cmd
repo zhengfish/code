7683389ec0200bb3cf9b98caf420c029c29093ca
zhengfish <zhengfish@gmail.com>
2018-09-11 14:58:51 +0800

    Add a build batch command for filezilla server for both dll_release and static_release
    Signed-off-by: zhengfish <zhengfish@gmail.com>

--- filezilla/build_all.cmd
+++ filezilla/build_all.cmd
@@ -11,7 +11,9 @@ nmake -f win32/Makefile.msc
 
 @echo build libfilezilla.lib
 cd /d %~dp0
-msbuild libfilezilla/lib/libfilezilla.vcxproj /t:Build /p:Configuration=dll_release;Platform=x86
+cd libfilezilla
+msbuild lib/libfilezilla.vcxproj /t:Build /p:Configuration=dll_release;Platform=x86
+msbuild lib/libfilezilla.vcxproj /t:Build /p:Configuration=static_release;Platform=x86
 
 @echo build filezillaserver.exe
 cd /d %~dp0
